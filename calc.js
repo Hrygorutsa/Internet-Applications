c8d4239243b38456f0133584da0b798c5d0e3e76
Ivan Hryhorutsa <ivanhrygorutsa@gmail.com>
2023-03-21 15:50:44 +0100

    Initial JS file added

---
+++ calc.js
@@ -0,0 +1,13 @@
+// function init(){
+//     const container = document.getElementById(container);
+//     console.log(container);
+// }
+
+// window.addEventListener("DOMContentLoaded",init);
+
+window.addEventListener("DOMContentLoaded",()=>{
+    const container = document.getElementById("container");
+    console.log();
+}
+);
+
